5f362c88d4a88e01ed3b92de16199eb87d062144
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-06-04 01:13:38 -0700

    Big Commit. Imported AI path finding so that enemies could chase you. I also added some UI. The health is shown on the top left and the coins are on the top right. A coin is added every X seconds

--- Survive The Spike/Assets/Scripts/Player.cs
+++ Survive The Spike/Assets/Scripts/Player.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] int health = 100;
+    [SerializeField] TextMeshProUGUI healthText;
+
+    private void Awake() {
+        if (healthText == null)
+            Debug.LogError("Can not find reference to health text!");
+    }
 
     void Update(){
-        //Debug.Log("HEALTH: " + health);
+        healthText.text = "Health: " + health.ToString();
     }
 
     public void DamagePlayer(int _damage) {
         health -= _damage;
     }
+
     public IEnumerator DamageLeach (int _damagePerSec, float _durration){
         for(int i = 0; i < _durration; i++){
         DamagePlayer(_damagePerSec);
--- Survive The Spike/Assets/Scripts/gameMaster.cs
+++ Survive The Spike/Assets/Scripts/gameMaster.cs
@@ -1,29 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class gameMaster : MonoBehaviour
 {
     Player playerScript;
     playerMovement playerMovementScript;
-    
+    [SerializeField] TextMeshProUGUI coinText;
+    [SerializeField] int addCoinInterval = 6;
+    float timeToAddCoin;
 
+
+
+    private int coinCount = 0;
     bool isLeaching = false;
     //this bool is used to make sure that leaching is not overlapped (called again before it is finished)
+
+    
 
     private void Awake() {
         playerScript = FindObjectOfType<Player>();
         playerMovementScript = FindObjectOfType<playerMovement>();
+        timeToAddCoin = (float)addCoinInterval;
     }
 
     private void Start() {
         if(playerScript == null) 
             Debug.LogError("Can't find object of type: player");
+        if(coinText == null) 
+            Debug.LogError("Can't find reference to coin text UI");
 
         if (playerMovementScript == null) 
             Debug.LogError("Can't find object of type: playerMovement");
     }
 
+    private void Update() {
+        coinText.text = "Coins: " + coinCount.ToString();
+        ConstantlyAddCoins();
+
+    }
+
+    private void ConstantlyAddCoins() {
+        //coinCount = (int)(Mathf.Floor(Time.time) * .25f);
+        
+        if(Time.time > timeToAddCoin) {
+            coinCount++;
+            timeToAddCoin = addCoinInterval + Time.time;
+        }
+
+    }
 
     public void Leach(int durration, int damageASec, float moveMultiply, int moveAdd, bool ispermanent, Transform projectile = null){
         
@@ -35,10 +61,7 @@ public class gameMaster : MonoBehaviour
             Destroy(projectile.gameObject);
             Debug.Log(projectile.gameObject.name + " Destroyed because it hit player when player was already leaching");
             //make them have a small explosion or some effect
-        }
-
-        
-        
+        } 
     }
 
     IEnumerator isLeachControl(int durration, Transform _projectile) {
