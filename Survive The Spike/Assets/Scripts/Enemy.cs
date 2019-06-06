65eec58225258d07b7ec6923712da1060f7132c1
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-06-06 00:36:44 -0700

    Added in player spawning. Enemies now randmly spawn arround the map and the spawn rate as well as the max ammount of live eniemies slowly increases as time goes by

--- Survive The Spike/Assets/Scripts/Enemy.cs
+++ Survive The Spike/Assets/Scripts/Enemy.cs
@@ -7,33 +7,42 @@ public class Enemy : MonoBehaviour
     int enemyHealth = 100;
     [SerializeField] GameObject enemyExposion;
     [SerializeField] GameObject hitMarker;
+
     bool explodeOnce = false;
+    bool dead = false;
+    //These bools are so stuff only gets called once and not twice by mistake
+
 
     [SerializeField] int coinReward = 3;
 
     private void Update() {
         if(enemyHealth <= 0 && !explodeOnce) {
             KilledByPlayer(3);
-            FindObjectOfType<gameMaster>().addCoins(coinReward);
         }
     }
 
+
     private void OnCollisionEnter2D(Collision2D collision) {
         if(collision.collider.tag == "Player") {
+            //Enemie died because it touched
             transform.parent.GetComponent<Animator>().SetTrigger("startSlowDeath");
-            Debug.Log("Enemie died because it touched");
             FindObjectOfType<Player>().DamagePlayer(3);
-            
+            if (!dead) {
+                dead = true;
+                FindObjectOfType<gameMaster>().AddToLiveEnemyCounter(-1);
+            }
         }
     }
 
     void KilledByPlayer(int coinReward) {
+        //Enemie died because player killed him
         explodeOnce = true;
         GameObject explosionClone = Instantiate(enemyExposion, transform.position, Quaternion.identity);
-        Debug.Log("Enemie died because of player");
-
+        FindObjectOfType<gameMaster>().AddToLiveEnemyCounter(-1);
+        FindObjectOfType<gameMaster>().addCoins(coinReward);
         Destroy(explosionClone, .2f);
         Destroy(transform.parent.gameObject, .1f);
+
     }
 
     public void TakeDamage(int damage) {
--- Survive The Spike/Assets/Scripts/gameMaster.cs
+++ Survive The Spike/Assets/Scripts/gameMaster.cs
@@ -11,6 +11,28 @@ public class gameMaster : MonoBehaviour
     [SerializeField] int addCoinInterval = 6;
     float timeToAddCoin;
 
+    [Header("Enemy Spawning")]
+    [SerializeField] GameObject enemyPrefab;
+
+    [SerializeField] Transform topSpawnMarker;
+    [SerializeField] Transform rightSpawnMarker;
+    [SerializeField] Transform bottomSpawnMarker;
+    [SerializeField] Transform leftSpawnMarker;
+    
+    [SerializeField] int maxNumOfLiveEnemies = 0;
+    [SerializeField] int liveEnemyCount = 0;
+
+    [SerializeField] float maxEnemySpawnWidth = 5;
+    [SerializeField] float maxEnemySpawnHeight = 5;
+
+    [SerializeField] float EnemySpawnRate = 1.5f;
+    float timeToSpawnEnemy = 0;
+
+    [SerializeField] int raiseMaxLiveEnemiesInterval = 10;
+    float timeToRaiseMaxEnemies = 0;
+
+    // write method to constantly increase this like i did with the coins
+
 
 
     private int coinCount = 0;
@@ -26,6 +48,8 @@ public class gameMaster : MonoBehaviour
     }
 
     private void Start() {
+        //AKA void ErrorChecking() LOL
+
         if(playerScript == null) 
             Debug.LogError("Can't find object of type: player");
         if(coinText == null) 
@@ -33,12 +57,37 @@ public class gameMaster : MonoBehaviour
 
         if (playerMovementScript == null) 
             Debug.LogError("Can't find object of type: playerMovement");
+
+        if(enemyPrefab == null)
+            Debug.LogError("Can't find reference to Enemy Prefab");
+
+        if (topSpawnMarker == null)
+            Debug.LogError("Can't find reference to top marker");
+        if (rightSpawnMarker == null)
+            Debug.LogError("Can't find reference to right marker");
+        if (bottomSpawnMarker == null)
+            Debug.LogError("Can't find reference to bottom marker");
+        if (leftSpawnMarker == null)
+            Debug.LogError("Can't find reference to left marker");
+
     }
 
     private void Update() {
         coinText.text = "Coins: " + coinCount.ToString();
         ConstantlyAddCoins();
+        if (Time.time > timeToSpawnEnemy) {
+            EnemySpawning();
+            timeToSpawnEnemy = EnemySpawnRate + Time.time;
+        }
+        
+        ConstantlyRaiseMaxLiveEnemies();
+    }
 
+    private void ConstantlyRaiseMaxLiveEnemies() {
+        if (Time.time > timeToRaiseMaxEnemies) {
+            maxNumOfLiveEnemies++;
+            timeToRaiseMaxEnemies = raiseMaxLiveEnemiesInterval + Time.time;
+        }
     }
 
     private void ConstantlyAddCoins() {
@@ -72,7 +121,50 @@ public class gameMaster : MonoBehaviour
         isLeaching = true;
         yield return new WaitForSeconds(durration);
         _projectile.gameObject.GetComponent<Animator>().SetBool("willDestroy", true);
-        Debug.Log(_projectile.gameObject.name + " In FadeDestroy animation");
         isLeaching = false;
+    }
+
+    public void AddToLiveEnemyCounter(int ammount) {
+        liveEnemyCount += ammount;
+    }
+
+    void EnemySpawning() {
+        //Step 1: Make sure the that the ammount of alive enemies does not exceed the maxNumOfAliveEnemies
+        if(liveEnemyCount < maxNumOfLiveEnemies) {
+
+            AddToLiveEnemyCounter(1);
+            //Step 2: Choose which side the enemy will spawn. (Left, right, top, bottom)
+            int whichSide = Random.Range(1, 5);
+            Debug.Log(whichSide);
+            //Step 3: Choose where on that side the enemie will spawn.
+            float xPos = Random.Range(-maxEnemySpawnWidth, maxEnemySpawnWidth);
+            float yPos = Random.Range(-maxEnemySpawnHeight, maxEnemySpawnHeight);
+            Vector3 spawnPos = new Vector3(xPos, topSpawnMarker.position.y, topSpawnMarker.position.z);
+                //added a default just incase;
+
+            if (whichSide == 1) {
+                //TOP
+                spawnPos = new Vector3(xPos, topSpawnMarker.position.y, topSpawnMarker.position.z);
+            }
+            else if (whichSide == 2) {
+                //RIGHT
+                spawnPos = new Vector3(rightSpawnMarker.position.x, yPos, rightSpawnMarker.position.z);
+            }
+            else if (whichSide == 3) {
+                //BOTTOM
+                spawnPos = new Vector3(xPos, bottomSpawnMarker.position.y, bottomSpawnMarker.position.z);
+            }
+            else if (whichSide == 4) {
+                //LEFT
+                spawnPos = new Vector3(leftSpawnMarker.position.x, yPos, leftSpawnMarker.position.z);
+            }
+
+
+            //Step 4: Spawn Enemy
+            Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+            
+
+        }
+
     }
 }
